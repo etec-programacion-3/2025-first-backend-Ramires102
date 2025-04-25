9708e76ae152edab176c41f4239e5b984764f777
Ramires102 <p.chiavetta@alumno.etec.um.edu.ar>
2025-04-25 12:51:41 -0300

    meidados de terminar todo el CRUD

--- model/LIBROS.js
+++ model/LIBROS.js
@@ -1,1 +1,32 @@
- 
+ //importamos la libreria sequelize(DataTypes) para poder usar los tipos de datos
+ import {DataTypes} from 'sequelize';
+ //importamos la conexion a la base de datos
+ import {sequelize} from '../config/base_du_datos.js';
+
+ //definimos la base de lo que tiene que tener un libro en la tabla libros
+ const Libros = sequelize.define('libros', {
+        //id unico del libro (describimos como es el id)
+        ID: {
+            type: DataTypes.INTEGER,
+            primaryKey: true, //llave primaria: no se puede repetir numero y no se puede duplicar 
+            autoIncrement: true, //incrementa automaticamente el id para no repetirlo
+        },
+        //titulo del libro
+        Titulo: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        //autor del libro
+        Autor: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        //año de publicacion
+        año_publicacion: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    }, {
+        //nombre de la tabla de la base de datos
+        tableName: 'libros',
+    });
